85368165ba570aadb8662cb05d49dece1957d05d
harshy261 <iyadavharsh261@gmail.com>
2022-12-02 18:53:27 +0530

    Day 1 commit

--- Day2.py
+++ Day2.py
@@ -1,1 +1,10 @@
 #*********************  DAY 2  **********************
+
+# Project Showcase
+
+# 1. Zarvis Voice Assistant
+# 2. Love Calculator
+# 3. Face Regonition
+# 4. Flappy Bird
+# 5. Snake Game
+
---
+++ Day3.py
@@ -0,0 +1,10 @@
+# MODULES AND PIP
+
+# 1. Built-In Modules
+# 2. External Modules
+
+# REPL ---- Read Evaluate Print Loopx bvmmmmmmmmmmmc
+
+import pandas
+print("Hi")
+

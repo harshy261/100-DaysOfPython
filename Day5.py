e1d123f9dc35cdd276d940ecb309917978401052
harshy261 <iyadavharsh261@gmail.com>
2022-12-02 20:18:55 +0530

    Day5 Commit

---
+++ Day5.py
@@ -0,0 +1,16 @@
+#*********************  DAY 5  **********************
+
+
+# Comment 
+
+print("I am a good boy.\nThis is Second Line.")
+
+print()
+
+print("Hey I am a \"good boy\"\nand this viewer is also a good boy/girl")
+
+print("Hey", 6, 7, sep="~", end="009\n")
+
+print("Harsh")
+
+

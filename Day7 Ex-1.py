24ffef8b3fa7f82e78dffbc0923ec8d61c84b1b6
harshy261 <iyadavharsh261@gmail.com>
2022-12-06 15:14:51 +0530

    Day7 Commit

---
+++ Day7 Ex-1.py
@@ -0,0 +1,18 @@
+#*********************  DAY 7  **********************
+
+# Ex 1 --- Calculator using Python
+
+a = int(input("Enter Your first Number: "))
+b = int(input("Enter Your second Number: "))
+
+
+print("The Addition of numbers is : ", (a+b))
+print("The Subtraction of numbers is : ", (a-b))
+print("The Multiplication of numbers is : ", (a*b))
+print("The Division of numbers is : ", (a/b))
+print("The Remainder of numbers is : ", (a%b))
+print("The Power of numbers is : ", (a**b))
+print("The Divisor of numbers is : ", (a//b))
+
+
+

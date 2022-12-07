e641cbcd29d82573b72b0dbae685c127603584ca
harshy261 <iyadavharsh261@gmail.com>
2022-12-07 23:48:35 +0530

    Day10 Commit

---
+++ Day10.py
@@ -0,0 +1,14 @@
+# INPUT FROM USER
+
+a = input()
+print("My name is ", a)
+
+
+x = input("Enter Your Name : ")
+print(x)
+
+
+l = int(input("Enter your 1st Number : "))
+m = int(input("Enter your 2nd Number : "))
+
+print(l+m)

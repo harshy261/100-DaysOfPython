1424a932d201ff307ad86c026ae759012fc725eb
harshy261 <iyadavharsh261@gmail.com>
2022-12-08 20:31:34 +0530

    Day11 Commit

---
+++ Day11.py
@@ -0,0 +1,36 @@
+# STRINGS
+
+name = "Harsh"
+friend = "Ankit"
+secFriend = "Rohan"
+
+apple = "He said, \I want to eat an apple"
+print("Hello, "+ name)
+
+
+apple = '''HI HARSH, 
+I want to eat an APPLE
+and be healthy.'''
+print(apple)
+
+
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+
+
+print("Let's use a For Loop\n")
+
+for ch in name:
+    print(ch)
+
+for ch in apple:
+    print(ch)
+
+
+
+

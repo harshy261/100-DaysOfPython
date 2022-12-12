697c838f62023e99f5960446919d1af161724d79
harshy261 <iyadavharsh261@gmail.com>
2022-12-12 21:36:28 +0530

    Day14 commit

---
+++ Day14.py
@@ -0,0 +1,26 @@
+# If Else Condition
+
+a = int(input("Enter Your Number : "))
+print("Your age is:", a)
+
+print(a>18)
+print(a>=18)
+print(a<18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+
+
+
+if(a>18):
+    print("You can drive")
+    print("If condition")
+elif(a==18):
+    print("Wait one Year")
+    print("Elif Condition")
+else:
+    print("You cannot drive")
+    print("Else condition")
+print("Loop ended")
+
+

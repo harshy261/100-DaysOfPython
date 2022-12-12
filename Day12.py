e10a46c95d496316aa70410034ea5cc1a4410939
harshy261 <iyadavharsh261@gmail.com>
2022-12-12 20:32:51 +0530

    Day12 Commit

---
+++ Day12.py
@@ -0,0 +1,38 @@
+# STRING SLICING
+
+# name = "Harsh.Shubham"
+# print(len(name))
+# print(name[0:8])                  # [inclusive : exclusive]
+
+
+
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is", len1, "letter word.")
+
+# print(fruit[0:5])
+# print(fruit[0:6])
+# print(fruit[0:2])
+# print(fruit[1:4])
+# print(fruit[:2])
+# print(fruit[2:])
+# print(fruit[2:4])
+
+# print(fruit[-1:len[fruit]-3])
+# print(fruit[-3:len[fruit]-1])
+print(fruit[-3:-1])
+
+
+
+
+
+# QUICK QUIZ
+
+nm = "Harry"
+print(nm[-4:-2])                # ar
+
+
+
+
+
+

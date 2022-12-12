517c4f3b95b76895c9cb31af990648dee5fed53b
harshy261 <iyadavharsh261@gmail.com>
2022-12-12 21:21:32 +0530

    Day13 commit

---
+++ Day13.py
@@ -0,0 +1,74 @@
+# STRING METHODS
+
+a = "Harsh!!! Sangam!! !!!"
+
+# print(len(a))
+# print(a)
+# print(a.upper())
+# print(a.lower())
+# print(a.rstrip('!'))
+# print(a.replace("Harsh", "John"))
+# print(a.split(" "))
+
+# blogHeading = "introduction to js"
+# print(blogHeading.capitalize())
+
+
+# str1 = "Welcome to the Console !!!"
+# print(len(str1))
+# print(len(str1.center(50)))
+# print(str1.endswith("!!!"))
+
+# print(a.count("Harsh"))
+# print(str1.endswith("to", 4, 10))
+# print(str1.find("om"))
+# print(str1.find("shs"))
+
+# print(str1.index("isisi"))
+
+
+str1 = "Welcome to my Channel"
+print(str1.isalnum())
+
+str1 = "Welcome"
+print(str1.isalpha())
+
+str1 = "Welcome"
+print(str1.islower())
+
+str1 = "We wish you to Merry Christmas\n"
+print(str1.isprintable())
+
+str1 = "                \n"     # using spacebar
+print(str1.isspace())
+
+str1 = "        \n"             # using tab
+print(str1.isspace())
+
+str1 = "World Health Organisation"
+print(str1.istitle())
+
+str1 = "World Health Organisation"
+print(str1.startswith("world"))
+
+str1 = "World Health Organisation"
+print(str1.swapcase())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

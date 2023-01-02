5440a8365e85888507f527d3fb1787d7e5943cbc
harshy261 <iyadavharsh261@gmail.com>
2023-01-02 15:02:34 +0530

    Day17 Commit

--- Day17.py
+++ Day17.py
@@ -3,24 +3,3 @@
 # Similar to Switch Case in C++
 # Break is not compulsory in PYTHON
 
-
-
-
-
-
-
-x = int(input("Enter the value of x: "))
-
-match x:
-    case 0:
-        print("X is Zero")
-    case 2:
-        print("X is 2")
-    case 4:
-        print("X is 4")
-    case 6:
-        print("X is 6")
-    case _ if x!=20:
-        print("X is not 20")
-    case _:
-        print(x)
